b2d766c1e518a85508c2c325e74406325e756b21
Luke Flaherty <flaherty.luke@yahoo.com>
2026-07-09 08:22:42 -0400

    Override sitewide 80% site-main cap that was squeezing the PDP
    Root cause of the FAQ/PDP horizontal spacing issue was a Jetpack
    Additional CSS rule (DB-stored, outside this repo) capping .site-main
    at max-width:80% !important on every viewport. Adds a higher-specificity
    override scoped to single-product pages.

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@ add_action( 'wp_enqueue_scripts', function() {
 		'myogenix-home',
 		get_stylesheet_directory_uri() . '/assets/css/home.css',
 		[],
-		'1.4.1'
+		'1.4.2'
 	);
 	wp_enqueue_script(
 		'myogenix-home',
@@ -484,7 +484,7 @@ add_action( 'wp_enqueue_scripts', function() {
 			'myogenix-pdp',
 			get_stylesheet_directory_uri() . '/assets/css/pdp.css',
 			[],
-			'1.9.6'
+			'1.9.7'
 		);
 	}
 
